7094bda2523694a9ca732f0e3b0104a94f910f66
Davaria <Daprimovaria@outlook.com>
2020-04-26 18:18:39 -0500

    Variables en R

---
+++ concepts-basic/variables.r
@@ -0,0 +1,18 @@
+# Calcular el tiempo total de nuestras actividades
+Oficina <- 3
+Platzi <- 4
+Transporte <- 1.5
+
+Tiempo_al_dia  <- Oficina + Platzi + Transporte
+#Imprimimos el resultado
+Tiempo_al_dia 
+
+# Calculamos la nota promedio de un alumno cada alumno 
+First_score <- 15
+Second_score <- 12
+Third_score <- 18
+
+#Sumamos los tres numeros y los dividimos entre 3
+Final_score <- (First_score + Second_score + Third_score)/3
+
+Final_score
