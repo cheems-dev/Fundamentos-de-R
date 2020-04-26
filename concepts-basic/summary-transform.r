7804a5991f3121c993af4c2be7231edc14449539
Davaria <Daprimovaria@outlook.com>
2020-04-26 18:28:20 -0500

    Summary y transform

---
+++ concepts-basic/summary-transform.r
@@ -0,0 +1,9 @@
+#summary: función que nos va a mostrar un resumen del dataset que le mandemos.
+summary(mtcars)
+#Vamos a cambiar el peso de libras a kilos
+wt <-(mtcars$wt*1000)/2
+#Visualizamos la operacion realizada, pero no ejecuta ninguna modificacion
+wt
+#transform: función para modificar los valores de un dataset.
+mtcars.new <- transform(mtcars,wt=wt*1000/2)
+summary(mtcars.new)
